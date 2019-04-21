28827d0fd2c327237d3922e04e49910fea1514a6
HkeiWar <1050225782@qq.com>
2019-04-21 14:31:42 +0800

    feat：sdl_event

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "SDL.h"
 #include "SDL_image.h"
 #include "config.h"
+#include <stdio.h>
 
 //主界面
 static SDL_Window *game_window = NULL;
@@ -12,10 +13,13 @@ static SDL_Renderer *game_renderer = NULL;
 //纹理
 static SDL_Texture *game_texture = NULL;
 
+//游戏状态
+static int game_status = 1;
+
 /**
  * 加载图片
  **/
-SDL_Texture *load_imgage(SDL_Renderer *renderer, const char *text)
+SDL_Texture *load_image(SDL_Renderer *renderer, const char *text)
 {
     IMG_Init(0);
     SDL_Texture *texture = NULL;
@@ -28,7 +32,7 @@ SDL_Texture *load_imgage(SDL_Renderer *renderer, const char *text)
     return texture;
 }
 
-SDL_Surface *load_imgage1(const char *text)
+SDL_Surface *load_image1(const char *text)
 {
     IMG_Init(0);
     SDL_Surface *surface = NULL;
@@ -51,7 +55,12 @@ static void quit();
 int main(int argc, char *args[])
 {
     init();
-    start_game();
+    while (game_status)
+    {
+        start_game();
+        SDL_Delay(100);
+    }
+
     quit();
     return 0;
 }
@@ -71,19 +80,32 @@ static void init()
 
 static void start_game()
 {
-    SDL_Surface *su = load_imgage1("assets\\test.bmp");
-    SDL_RenderClear(game_renderer);
-    SDL_UpdateTexture(game_texture, NULL, su->pixels, su->pitch);
-    SDL_RenderCopy(game_renderer, game_texture, NULL, NULL);
-    SDL_RenderPresent(game_renderer);
-    SDL_Delay(5000);
+    SDL_Event sdl_event;
 
-    SDL_Surface *su1 = load_imgage1("assets\\test1.bmp");
-    SDL_RenderClear(game_renderer);
-    SDL_UpdateTexture(game_texture, NULL, su1->pixels, su1->pitch);
-    SDL_RenderCopy(game_renderer, game_texture, NULL, NULL);
+    // SDL_Surface *su = load_imgage1("assets\\test.jpg");
+    // SDL_RenderClear(game_renderer);
+    // SDL_UpdateTexture(game_texture, NULL, su->pixels, su->pitch);
+    // SDL_RenderCopy(game_renderer, load_image(game_renderer,"./assets/test.jpg"), NULL, NULL);
+    SDL_SetRenderDrawColor(game_renderer, 0, 255, 0, 255);
+    SDL_Rect rect = {200, 300, 100, 100};
+    SDL_RenderDrawRect(game_renderer, &rect);
     SDL_RenderPresent(game_renderer);
-    SDL_Delay(5000);
+    SDL_PollEvent(&sdl_event);
+    if (sdl_event.type == SDL_KEYDOWN)
+    {
+        rect.x++;
+    }
+    if (sdl_event.type == SDL_KEYUP)
+    {
+        game_status = 0;
+    }
+    
+    // SDL_Surface *su1 = load_imgage1("assets\\test1.png");
+    // SDL_RenderClear(game_renderer);
+    // SDL_UpdateTexture(game_texture, NULL, su1->pixels, su1->pitch);
+    // SDL_RenderCopy(game_renderer, game_texture, NULL, NULL);
+    // SDL_RenderPresent(game_renderer);
+    // SDL_Delay(5000);
 }
 
 static void quit()
